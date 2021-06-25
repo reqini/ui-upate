76fb9e7146e334b0be2c9fb064d79e5acb53f8a0
Luciano Recchini <lucianorecchini@FBZ-MAC-003.local>
2021-06-25 17:47:40 -0300

    priceNav

--- src/stories/Navs/PriceNav.js
+++ src/stories/Navs/PriceNav.js
@@ -8,15 +8,15 @@ import Button from '../Button';
 
 import './PriceNav.scss';
 
-export const PriceNav = ({ promoType, name, price, night, people, direction, scroll, colorPromoType }) => (
+export const PriceNav = ({ promoType, name, price, night, people, direction, scroll, colorPromoType, board, boardColor }) => (
   <div className={`nav ${scroll && 'scroll'}`}>
     <div className="wrapper">
       <div className="info-hotel left">
         <Typography fontSize={12} color={colorPromoType} fontWeight="100" variant="span" margin="2px 0">{promoType}</Typography>
         <Typography variant="h3" fontSize={24} fontWeight="500" variant="h2">{name}</Typography>
-        <Typography variant="span" fontSize={15} margin="5px 0" className="flex-center"><RoomIcon fontSize="small" />{direction}</Typography>
-        <Button size={30} width={200} background="#4A90E2">
-          All Inclusive
+        <Typography variant="p" fontSize={15} margin="5px 0" className="flex-center"><RoomIcon fontSize="small" />{direction}</Typography>
+        <Button size={30} width={200} background={boardColor}>
+          {board}
         </Button>
       </div>
       <div className="info-hotel block-price-color center">
@@ -39,7 +39,9 @@ PriceNav.propTypes = {
   scroll: PropTypes.bool,
   night: PropTypes.number,
   people: PropTypes.number,
-  direction: PropTypes
+  direction: PropTypes,
+  board: PropTypes,
+  boardColor: PropTypes
 };
 
 PriceNav.defaultProps = {
@@ -48,6 +50,8 @@ PriceNav.defaultProps = {
   colorPromoType: '',
   name: 'Cristal Hotel de Ejemplo',
   direction: 'Av. Bustillo Km. 2.5',
+  board: 'All Inclusive',
+  boardColor: '#4A90E2',
   price: '1400',
   night: 3,
   people: 2
