8849f7aa020a9e7ca336760af47127bcc473a2f2
Luciano Recchini <lucianorecchini@FBZ-MAC-003.fibertel.com.ar>
2021-06-29 10:29:26 -0300

    PriceNav mobile

--- src/stories/Navs/PriceNav.js
+++ src/stories/Navs/PriceNav.js
@@ -8,23 +8,25 @@ import Button from '../Button';
 
 import './PriceNav.scss';
 
-export const PriceNav = ({ promoType, name, price, night, people, direction, scroll, colorPromoType, board, boardColor }) => (
+export const PriceNav = ({ promoType, name, price, night, people, direction, scroll, colorPromoType, board, boardColor, action }) => (
   <div className={`nav ${scroll && 'scroll'}`}>
     <div className="wrapper">
       <div className="info-hotel left">
         <Typography fontSize={12} color={colorPromoType} fontWeight="100" variant="span" margin="2px 0">{promoType}</Typography>
-        <Typography variant="h3" fontSize={24} fontWeight="500" variant="h2">{name}</Typography>
+        <Typography variant="h3" fontWeight="500" variant="h2">{name}</Typography>
         <Typography variant="p" fontSize={15} margin="5px 0" className="flex-center"><RoomIcon fontSize="small" />{direction}</Typography>
         <Button size={30} width={200} background={boardColor}>
           {board}
         </Button>
       </div>
       <div className="info-hotel block-price-color center">
-        <Typography fontSize={16} fontWeight="400" variant="p">Total final</Typography>
-        <Typography variant="h3" fontSize={30} fontWeight="700">${price}</Typography>
-        <Typography fontSize={12} fontWeight="100" variant="span" margin="5px 0">{night} noche, {people} personas</Typography>
-        <Button size={48} width={'100%'}>
-          CONSULTAR
+        <div className="w-full">
+          <Typography fontSize={16} fontWeight="400" variant="p">Total final</Typography>
+          <Typography variant="h3" fontSize={30} fontWeight="700">${price}</Typography>
+          <Typography fontSize={12} fontWeight="100" variant="span" margin="5px 0">{night} noche, {people} personas</Typography>
+        </div>
+        <Button size={48} onClick={action}>
+          VER HABITACIONES
         </Button>
       </div>
     </div>
@@ -41,10 +43,12 @@ PriceNav.propTypes = {
   people: PropTypes.number,
   direction: PropTypes,
   board: PropTypes,
-  boardColor: PropTypes
+  boardColor: PropTypes,
+  action: PropTypes
 };
 
 PriceNav.defaultProps = {
+  action: null,
   scroll: false,
   promoType: 'Descuento exclusivo',
   colorPromoType: '',
